69f60293d66695f72618bb95eb810d4153bf64d0
drdaang <99331378+drdaang@users.noreply.github.com>
2024-07-16 12:03:42 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 module.exports = () => {
-    const connectionParams = {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-    try {
-        mongoose.connect("mongodb+srv://vercel-admin-user:Abhinav@2021>@cluster0.hcpv2ij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", connectionParams);
-        console.log("Connected to database successfully")
-    } catch (err) {
-        console.log(err);
-        console.log("Failed to connect to database");
-    }
-}
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+  try {
+    mongoose.connect(
+      "mongodb+srv://vercel-admin-user:Abhinav%402021@cluster0.hcpv2ij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+      connectionParams
+    );
+    console.log("Connected to database successfully");
+  } catch (err) {
+    console.log(err);
+    console.log("Failed to connect to database");
+  }
+};
